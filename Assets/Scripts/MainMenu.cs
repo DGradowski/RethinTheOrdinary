1aa94049544c53c0b09813132683ac6bc80c0d22
gugapl <ogorekigor@gmail.com>
2025-07-09 00:31:16 +0200

    added few things to menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,16 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.IO;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene("Level 1");
-    }
+    public Button[] buttons;
 
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OpenLevel(int levelId)
+    {
+        string levelName = "Level " + levelId;
+        if (SceneExists(levelName))
+        {
+            SceneManager.LoadScene(levelName);
+        }else
+        {
+            Debug.LogWarning($"\"{levelName}\" is not available");
+        }
+        
+    }
+
+    // Funkcja sprawdzaj¹ca czy scena o danej nazwie istnieje 
+    private bool SceneExists(string sceneName)
+    {
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+
+        for (int i = 0; i < sceneCount; i++)
+        {
+            string path = SceneUtility.GetScenePathByBuildIndex(i);
+            string name = Path.GetFileNameWithoutExtension(path);
+
+            if (name == sceneName)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
