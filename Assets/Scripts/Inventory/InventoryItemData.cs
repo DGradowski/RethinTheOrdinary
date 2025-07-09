d03b34a1be9f7848e848f93258f586f673120fc4
Dawid Gradowski <251524@edu.p.lodz.pl>
2025-07-09 13:50:05 +0200

    Remove row and column attribute from InventoryItemData

--- Assets/Scripts/Inventory/InventoryItemData.cs
+++ Assets/Scripts/Inventory/InventoryItemData.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class InventoryItemData : ScriptableObject
 {
-	private int row = 0;
-	private int column = 0;
 	private int width = 0;
 	private int height = 0;
 	private string shape = string.Empty;
@@ -14,18 +12,14 @@ public class InventoryItemData : ScriptableObject
 
 	//TODO: ItemType
 
-	public InventoryItemData(int row, int column, int width, int height, string shape, Sprite sprite)
+	public InventoryItemData(int width, int height, string shape, Sprite sprite)
 	{
-		this.row = row;
-		this.column = column;
 		this.width = width;
 		this.height = height;
 		this.shape = shape;
 		this.sprite = sprite;
 	}
 
-	public int Row { get => row; }
-	public int Column { get => column; }
 	public int Width { get => width; }
 	public int Height { get => height; }
 	public Sprite Sprite { get => sprite; }
